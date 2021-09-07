9d87315f064f562b2c76c74274b9f83516f02277
Shiva Prasad <shivaprasad.m@sonata-software.com>
2021-09-07 08:44:40 +0530

    second assignment

---
+++ Day2 assignment/circle/circle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace circle
+{
+    class circle
+    {
+        int radius;
+        public circle()
+        {
+            Console.WriteLine("Enter radius...");
+            radius = int.Parse(Console.ReadLine());
+        }
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+        public int GetRadius()
+        {
+            return this.radius;
+        }
+        public int calcDiameter()
+        {
+            return radius * 2;
+        }
+        public double calcArea()
+        {
+            return (3.14 * radius * radius);
+        }
+    }
+    class CircleTest
+    {
+        public static void Main(string[] args)
+        {
+            circle Circle1 = new circle();
+            Console.WriteLine("The radius of circle is...{0}", Circle1.GetRadius());
+           
+            Console.WriteLine("The diameter of circle is...{0}", Circle1.calcDiameter());
+            
+            Console.WriteLine("The area of circle is...{0}",Circle1.calcArea());
+            
+        }
+    }
+    
+}
